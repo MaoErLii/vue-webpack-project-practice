7f9f74ee1b8292e9fb9692d0dd10427bf818b0a8
“MaoErLi <2844735407@qq.com>
2019-02-22 21:34:58 +0800

    引入iview ui库

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import iView from 'iview'
+
+import 'iview/dist/styles/iview.css'
 
 Vue.config.productionTip = false
 
+Vue.use(iView)
+
+// new Vue({
+//   el: '#app',
+//   component: { App },
+//   template: '<App/>'
+// })
 new Vue({
   render: h => h(App),
 }).$mount('#app')
